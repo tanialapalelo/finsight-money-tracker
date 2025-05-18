6544da1b82b8cf8c535ae727e95117e2e889b64e
Tania Lapalelo <niatania102@gmail.com>
2025-05-18 13:15:14 +0700

    feat: implement initial file and folder structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header"> 
+          
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Home
