830e91ec35c630e06bf094faf2bd15b226cd0b7b
Tania Lapalelo <niatania102@gmail.com>
2025-05-20 07:14:03 +0700

    feat: implement home page ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
+  const loggedIn = { firstname: "tania" };
   return (
     <section className="home">
       <div className="home-content">
-        <header className="home-header"> 
-          
+        <header className="home-header">
+          <HeaderBox type="greeting" title="Welcome" user={loggedIn.firstname || "Guest"} subtext="Access and manage your transaction" />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1235}/>
         </header>
       </div>
     </section>
