f209805be99c77fa9bdd4d7fa7d3e1dd16ec75a2
Tania Lapalelo <niatania102@gmail.com>
2025-06-06 15:58:57 +0700

    feat(auth): email password auth with redis for session store

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,24 @@
+import { getCurrentUser } from "@/auth/nextjs/currentUser";
 import HeaderBox from "@/components/HeaderBox"
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
-const Home = () => {
-  const loggedIn = { firstname: "tania" };
+const Home = async () => {
+  const loggedIn = await getCurrentUser({ withFullUser: true })
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox type="greeting" title="Welcome" user={loggedIn.firstname || "Guest"} subtext="Access and manage your transaction" />
+          <HeaderBox type="greeting" title="Welcome" user={loggedIn!.firstName || "Guest"} subtext="Access and manage your transaction" />
           <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1235}/>
         </header>
       </div>
 
-      {/* <RightSidebar 
-        user={loggedIn}
-        transactions={account?.transactions}
-        banks={accountsData?.slice(0, 2)}
-      /> */}
+      <RightSidebar 
+        user={loggedIn!}
+        // transactions={account?.transactions}
+        // banks={accountsData?.slice(0, 2)}
+      />
     </section>
   )
 }
